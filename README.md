5f9d409f4521db687fd15f24caed8ebda330b0c8
OlegK <oleg.korolenko@gmail.com>
2024-10-13 11:24:21 +0100

    unique id impl

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # maelstrom-rust
 
 [Fly.io distributed system challenges](https://fly.io/dist-sys/1/)  to play with Rust
+
+## To test
+
+Run from your maelstrom installation directory
+
+### echo
+
+```sh
+./maelstrom test -w unique-ids --bin ../maelstrom-rust/target/debug/unique-id --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition --log-stderr
+```
+
+### unique-id
+
+```sh
+./maelstrom test -w unique-ids --bin ../maelstrom-rust/target/debug/unique-id --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition --log-stderr
+```
--- src/bin/unique-id.rs
+++ src/bin/unique-id.rs
@@ -1,6 +1,6 @@
 use anyhow::anyhow;
 use anyhow::Result;
-use maelstrom_rust::msg_protocol::{Message, Processor};
+use maelstrom_rust::msg_protocol::*;
 use maelstrom_rust::runner::*;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
@@ -42,10 +42,36 @@ impl Processor<UniqueIdMessage> for UniqueIdGeneratorMaelstromNode {
         msg: Message<UniqueIdMessage>,
     ) -> Result<Option<Message<UniqueIdMessage>>> {
         match msg.body.body {
-            UniqueIdMessage::Init { node_id, node_ids } => todo!(),
-            UniqueIdMessage::InitOk {} => todo!(),
-            UniqueIdMessage::Generate {} => todo!(),
-            UniqueIdMessage::GenerateOk { id } => todo!(),
+            UniqueIdMessage::Init {
+                node_id: _,
+                node_ids: _,
+            } => {
+                // TODO fix repetition with other nodes
+                let reply = Ok(Some(Message {
+                    src: msg.dest,
+                    dest: msg.src,
+                    body: Body {
+                        msg_id: Some(self.id),
+                        in_reply_to: msg.body.msg_id,
+                        body: UniqueIdMessage::InitOk {},
+                    },
+                }));
+                self.id += 1;
+                reply
+            }
+            UniqueIdMessage::Generate {} => {
+                let reply = Ok(Some(Message {
+                    src: msg.dest,
+                    dest: msg.src,
+                    body: Body {
+                        msg_id: Some(self.id),
+                        in_reply_to: msg.body.msg_id,
+                        body: UniqueIdMessage::GenerateOk { id: Uuid::new_v4() },
+                    },
+                }));
+                self.id += 1;
+                reply
+            }
             _ => Err(anyhow!("Received unknown message: {:?}", msg)),
         }
     }
